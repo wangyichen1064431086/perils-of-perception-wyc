fde73b4bc8deae0fd4f7645c50929026c63809ac
wangyichen1064431086 <1064431086@qq.com>
2017-09-05 18:22:25 +0800

    Start to learn and write react

---
+++ client/components/question/index.js
@@ -0,0 +1,177 @@
+import React, { Component } from 'react';
+import Range from '../question-imputs/range';
+import ColumnChart from '../question-outputs/column-chart';
+
+class Question extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      answered: false,
+      value: null
+    }
+
+    this.markQuestion = this.markQuestion.bind(this);
+  }
+
+  markQuestion(event, value) {
+    const difference = Math.abs(this.props.answer - value);
+    let questionValue;
+
+    if(event) {
+      event.preventDefault();
+    }
+
+    this.setState({
+      answered: true,
+      value
+    });
+
+    if (difference < 15) {
+      questionValue = difference === 0 ? 
+      questionValue = 100 / this.props.questionsLength : 
+      questionValue = (100 / this.props.questionsLength) * (1 - (difference / 15))
+    } else {
+      questionValue = 0 ;
+    }
+
+    this.props.updateScore(questionValue);//该方法在index.js/class App中定义
+
+    fetch(`${this.props.endpoint}/response/`, {
+      methods: 'POST',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        value,
+        submitted: Date.now(),
+        questionId: this.props.questionId,
+        meta: {
+          Country: this.props.country
+        }
+      })
+    }).then(res => console.log(res))
+    .catch(e => console.error(e));
+
+    this.props.updateProgress(this.props.questionIndex + 1);
+  }
+
+  render() {
+    const rangeMin = this.props.options[0];
+    const rangeMax = this.props.options[1];
+    const activeClass = this.props.active ? ' active' : '';
+    const answeredClass = this.state.answered ? ' answered' : '';
+
+    const input = (
+      <Range
+        min={rangeMin}
+        max={rangeMax}
+        step={rangeMax / 100}
+        thumbSize={28}
+        onSubmit={this.markQuestion}
+      />
+    );
+    const chart = this.state.answered && (
+      <ColumnChart
+        data={this.props.responsesData}
+        initialWidth={this.node.offsetWidth}
+        inputMin={rangeMin}
+        inputMax={rangeMax}
+        userAnswer={this.state.value}
+        actualAnswer={this.props.answer}
+        countryAnswer={this.props.countryAnswer}
+      />
+    );
+    const output = this.state.answered && (
+      <div className="o-grid-container">
+        <div className="o-grid-row">
+          <div data-o-grid-colspan="12 M4">
+            <div className="legend">
+              <svg width="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="5" cy="5" r="5" className="legend-actual"/>
+              </svg>
+              <p className="o-typography-lead--small">实际情况</p>
+            </div>
+            <p>
+              答案是 <strong> {this.props.answer}&#37;</strong>.
+            </p>
+          </div>
+          <div data-o-grid-colspan="12 M4">
+            <div className="legend">
+              <svg width="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="5" cy="5" r="5" className="legend-user" />
+              </svg>
+              <p className="o-typography-lead--small">你的回答</p>
+            </div>
+            <p>
+              你认为是<strong>{this.state.value}&#37;</strong>.
+            </p>
+          </div>
+          <div data-o-grid-colspan="12 M4">
+            <div className="legend">
+              <svg width="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="5" cy="5" r="5" className="legend-country" />
+              </svg>
+              <p className="o-typography-lead--small">问卷调查结果</p>
+            </div>
+            <p>
+              调查民众答案 <strong>{this.props.countryAnswer}&#37;</strong>.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+    const crossheadLookup = {
+      19: '当前穆斯林人口',
+      20: '未来穆斯林人口',
+      21: '健康保险支出',
+      22: '拥有房产的比例',
+      23: '财富分配',
+      24: '幸福感',
+      26: '对堕胎的态度',
+      27: '对同性恋的态度',
+      28: '对婚前性行为的态度',
+    };
+    const crosshead = crossheadLookup[this.props.questionId];
+
+    return (
+      <div 
+        ref={ node => { this.node = node; }}
+        className={`question${activeClass}${answeredClass}`}
+        >
+        <h2 className="o-typography-subhead--crosshead">
+          {this.props.questionIndex + 1}. {crosshead}
+        </h2>
+
+        <p className="o-typography-lead--small">
+          {this.props.questionText}
+        </p>
+
+        {input}
+
+        {chart}
+
+        {output}
+      </div>
+    );
+  }
+}
+
+Question.propTypes = {
+  questionId: React.PropTypes.number,
+  questionIndex: React.PropTypes.number,
+  active: React.PropTypes.bool,
+  questionType: React.PropTypes.string,
+  questionText: React.PropTypes.string,
+  options: React.PropTypes.array,
+  answer: React.PropTypes.any,
+  countryAnswer: React.PropTypes.number,
+  responsesData: React.PropTypes.object,
+  updateProgress: React.PropTypes.func,
+  updateScore: React.PropTypes.func,
+  endpoint: React.PropTypes.string,
+  country: React.PropTypes.string,
+  questionsLength: React.PropTypes.number,
+};
+
+export default Question;
--- client/index.js
+++ client/index.js
@@ -5,13 +5,15 @@ import Question from './components/question';
 import Overlay from './components/overlay';
 
 Share.init();
+const endpoint = 'https://ft-ig-answer-api.herokuapp.com/api/v1';//该链接打开为not found
+
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props); //In ES6 Class, you need to explicitly call super(); when defining the constructor of a subclass.
     const {questions} = props;
 
-    this.state = {
+    this.state = { // In constructor, initialize the state:
       questionsLoaded: false,
       questions: [],
       questionsLength: 0,
@@ -37,11 +39,18 @@ class App extends Component {
         questionsLoaded: true,
         questions,
         country:value,
-        questionsLength: questions.filter(question => question.answer !== '').sort((a,b) => Number(a.meta.qid.slice(1)) - Number(b.meta.qid.slice(1))).slice(2).questionsLength
+        questionsLength: questions.
+        filter(question => question.answer !== '')//问题过滤，只要每项question.answer有内容的
+        .sort((a,b) => Number(a.meta.qid.slice(1)) - Number(b.meta.qid.slice(1)))//问题排序，根据每项question的.meta.qid.slice(1)的大小进行排序
+        .slice(2)//从第3个问题开始选取
+        .length//求前述过滤后剩下的问题数目
       }));      
   }
 
   updateProgress(n) {
+    /**
+     * @dest 如果n比questionsLength小，则设置activeQuestion为n;否则activeQuestion为null
+     */
     if(n < this.state.questionsLength) {
       this.setState({ activeQuestion: n });
     } else {
@@ -53,12 +62,85 @@ class App extends Component {
   }
 
   updateScore(n) {
-    this.setState(prevState => ({
+    /**
+     * @dest 为state.score增加分值n
+     */
+    this.setState(prevState => ({//this.setState(): schedule updates to the component local state
       score: prevState.score + n
     }));
   }
 
   render() {
- 
+    /**
+     * The render method returns a description of what you want to render, and then React takes that description and renders it to the screen. In particular, render returns a React element.
+     * Most React developers use JSX which makes it easier to write these structures.
+     * JSX： The <div /> syntax is transformed at build time to React.createElement('div').
+     * The render method can display the value from the current state 
+     */
+    const chooseQuestions = this.state.chooseQuestions && (
+      <Overlay setQuestions = {this.setQuestions}/>
+    );
+
+    const loadStatus = !this.state.questionsLoaded && <p><strong>测验正在快速加载...（若无法加载成功，建议使用Chrome、Firefox、Safari或IE 9以上的浏览器浏览。）</strong></p>
+
+    const questions = this.state.questions.filter(question => question.answer !== '')
+    .sort((a,b) => Number(a.meta.qid.slice(1)) - Number(b.meta.qid.slice(1)))
+    .slice(2)
+    .map((question, i) =>
+      <Question 
+          key={quesiton.meta.qid}
+          questionId={question.id}
+          questionIndex={i}
+          active={i === this.state.activeQuestion}
+          questionType={question.meta.type}
+          questionText={question.text}
+          options={
+            Object.keys(question.options)//Object.keys() 方法会返回一个由一个给定 对象的自身可枚举属性组成的数组
+            .map(option =>  
+              question.options[option]
+            ).filter(option =>
+              option !== null
+            )
+          }
+          answer={Number(question.answer)}
+          countryAnswer={Number(question.meta.perceived)}
+          responsesData={question.responses}
+          updateProgress={this.updateProgress}
+          updateScore={this.updateScore}
+          endpoint={endpoint}
+          country={this.state.country}
+          questionsLength={this.state.questionsLength}
+      />
+    );
+    let feedback;
+
+    switch (true) {
+      case this.state.score >= 70:
+        feedback = '- 你果然是达人';
+        break;
+      default:
+        feedback = '';
+
+    }
+
+    const results = this.state.complete && (
+      <div className="results">
+        <h2></h2>
+        <h2>
+          测验结束：你的准确率达{Math.round(this.state.score)}&#37;{feedback}
+        </h2>
+      </div>
+    )//&#37为%
+
+    return (
+      <div>
+        <link rel="stylesheet" href="https://build.origami.ft.com/v2/bundles/css?modules=o-buttons@^4.4.1" />
+
+        {chooseQuestions} 
+        {loadStatus}
+        {questions}
+        {results}
+      </div>
+    )
   }
 }
